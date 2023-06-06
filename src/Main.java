bf2df9dad1f5dc0b21df1a0202052cd9ccde4fc5
VladSoap <@pLUnkmdsV43TX9>
2023-06-07 00:15:09 +0300

    hw2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int dayOfWeek = 2;
+        switch (dayOfWeek) {
+            case 1 -> System.out.println("Понедельник");
+            case 2 -> System.out.println("Вторник");
+            case 3 -> System.out.println("Среда");
+            case 4 -> System.out.println("Четверг");
+            case 5 -> System.out.println("Пятница");
+            case 6 -> System.out.println("Суббота");
+            case 7 -> System.out.println("Воскресенье");
+            default -> System.out.println("Неверный день недели");
         }
     }
 }
